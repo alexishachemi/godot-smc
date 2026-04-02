12b88bc8dc56b1ec4a722d612ddf09b4165af8b5
Alexis <alexishachemi@gmail.com>
2026-04-02 14:10:21 +0900

    Update state label

--- addons/smc/utils/state_label.gd
+++ addons/smc/utils/state_label.gd
@@ -1,4 +1,5 @@
-class_name StateLabel
+@icon("../icons/icon_state_label.png")
+class_name SMCStateLabel
 extends Label
 
 ## A custom label that displays states
@@ -8,25 +9,25 @@ extends Label
 ## sibbling and use its internal state group if found.
 
 @export var prefix: String = ""
-@export var state_group: StateGroup = null
+@export var state_group: SMCStateGroup = null
 
-func _ready():
+func _ready() -> void:
 	if state_group == null:
 		for child in get_parent().get_children():
-			if child is StateMachine:
-				state_group = child.internal_group
+			if child is SMCStateMachine:
+				state_group = child._internal_group
 				_set_state(child.initial_state)
 				break
 	if state_group == null:
 		text = "[no group/machine]"
 	else:
-		state_group.state_changed.connect(_on_state_changed)
+		state_group.current_state_changed.connect(_on_state_changed)
 
-func _set_state(state_name: StringName):
+func _set_state(state_name: StringName) -> void:
 	if prefix != "":
 		text = "%s: %s" % [prefix, state_name]
 	else:
 		text = state_name
 
-func _on_state_changed(state_name: StringName):
-	_set_state(state_name)
+func _on_state_changed(_old: SMCState, new: SMCState) -> void:
+	_set_state(new.name)
